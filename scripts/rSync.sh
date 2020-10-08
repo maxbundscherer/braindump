80305fe1e5412ae9f6721abd23f411d776695a07
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-10-08 18:56:18 +0200

    add new script

---
+++ scripts/rSync.sh
@@ -0,0 +1,30 @@
+echo
+echo "----- Init FROM PATH (without / at end) -----"
+echo $1
+ls -all $1
+echo "Memory:"
+du -sh $1
+
+echo
+echo "----- Init FROM PATH (without / at end) -----"
+echo $2
+ls -all $2
+echo "Memory:"
+du -sh $2
+
+echo
+echo "----- Processed path -----"
+fromPath="$1/"
+toPath="$2/"
+echo "From Path (with / at end): '${fromPath}'"
+echo "To Path (with / at end): '${toPath}'"
+
+echo
+echo "PLEASE WAIT 15 s"
+sleep 15
+
+#read  -n 1 -p "Please press okay:" mainmenuinput
+
+echo "-Start COPY NOW-"
+
+sudo rsync -r -l -q -h --progress --stats $fromPath $toPath
