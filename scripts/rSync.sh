28dd9c86811ebe45a925dd02ef1b25f3cc5bfac0
Maximilian Bundscherer <maximilian@bundscherer-online.de>
2020-10-29 16:50:39 +0100

    add rSync

--- scripts/rSync.sh
+++ scripts/rSync.sh
@@ -1,16 +1,17 @@
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root"
+  exit
+fi
+
 echo
 echo "----- Init FROM PATH (without / at end) -----"
 echo $1
 ls -all $1
-#echo "Memory:"
-#du -sh $1
 
 echo
 echo "----- Init FROM PATH (without / at end) -----"
 echo $2
 ls -all $2
-#echo "Memory:"
-#du -sh $2
 
 echo
 echo "----- Processed path -----"
@@ -20,13 +21,17 @@ echo "From Path (with / at end): '${fromPath}'"
 echo "To Path (with / at end): '${toPath}'"
 
 echo
-echo "Please start script with sudo!!!"
 echo "PLEASE WAIT 15 s"
 sleep 15
 
-#read  -n 1 -p "Please press okay:" mainmenuinput
-
+echo
 echo "-Start COPY NOW-"
 
 rm rSyncSuc.log rSyncErr.log
-sudo rsync -r -l -h --progress --stats $fromPath $toPath 1> rSyncSuc.log 2> rSyncErr.log &
+
+echo "Start Copy" >> rSyncSuc.log
+echo $fromPath >> rSyncSuc.log
+echo $toPath >> rSyncSuc.log
+echo >> rSyncSuc.log
+
+sudo rsync -a -h --progress --stats $fromPath $toPath 1> rSyncSuc.log 2> rSyncErr.log &
